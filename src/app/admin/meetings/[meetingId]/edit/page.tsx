35008887ead5af881d08d5607a6be88e87d705b2
preeti saini <preetisaini@MacBookAir.lan>
2025-04-01 15:16:52 +0530

    a

--- src/app/admin/meetings/[meetingId]/edit/page.tsx
+++ src/app/admin/meetings/[meetingId]/edit/page.tsx
@@ -1,11 +1,10 @@
 'use client'
 import { Box } from '@mui/material'
 import axios from 'axios';
-import React, { useEffect, useState, } from 'react'
+import React, { useEffect, useState } from 'react'
 import MeetingForm from '../../components/CreateMeetings'
-import {useParams, useRouter} from 'next/navigation';
+import {useRouter} from 'next/navigation';
 import { ADD_MEETING_API, ADMIN_MEETING_ROUTE, GET_MEETING_API } from '@/constant';
-import useRequest from '@/util/useRequest';
 
 interface DataItem {
   title: string;
@@ -17,46 +16,38 @@ interface DataItem {
   type: string;
 }
 
-const Edit = ()=>{
-  const MeetingId = useParams<{meetingId: string}>()
-console.log(MeetingId)
-
-// ({
-//   params,
-// }: {
-//   params: Promise<{ meetingId: string }>;
-// })  {
+function Edit({
+  params,
+}: {
+  params: Promise<{ meetingId: string }>;
+})  {
 
 
   const router = useRouter()
   const [initialValues, setInitialValues] = useState<DataItem | null >(null);
-  // const [isLoading, setIsLoading] = useState(false)
+  
+  
+  useEffect( () => {
+        const fetchMeeting = async () => {
+          try {
+            
+            const response = await axios.get(`${GET_MEETING_API}/${(await params).meetingId}`);
+            const data = response.data.meeting;
+            // console.log(data)
+            setInitialValues(data);
 
-  // useEffect( () => {
-  //       const fetchMeeting = async () => {
-  //         try {
-  //           const response = await axios.get(`${GET_MEETING_API}${MeetingId}`);
-  //           console.log(response)
-  //           const data = response.data.meeting;
-  //           // console.log(data)
-  //           setInitialValues(data);
-
-  //         } catch (error) {
-  //           console.error('Error fetching meeting data:', error);
-  //         }
-  //       };
+          } catch (error) {
+            console.error('Error fetching meeting data:', error);
+          }
+        };
     
-  //        fetchMeeting();
-  //     }, [MeetingId]);
+         fetchMeeting();
+      }, [params]);
 
-const {data,isLoading} = useRequest({
-url:`${GET_MEETING_API}/${MeetingId}`
-})
-const upDate = data.meeting
-
-   const handleSubmit = async (values: any) => {  
+        const handleSubmit = async (values: any) => {
+      
             try {    
-              const response = await axios.put(`${ADD_MEETING_API}/${MeetingId}`,
+              const response = await axios.put(`${ADD_MEETING_API}/${(await params).meetingId}`,
               {
                 title: values.title,
                 description: values.description,
@@ -70,7 +61,7 @@ const upDate = data.meeting
               setInitialValues(response.data.meeting);
               
               if (response.status === 200) {
-                  router.push(`${ADMIN_MEETING_ROUTE.url}/${MeetingId}/details`);
+                  router.push(`${ADMIN_MEETING_ROUTE.url}/${(await params).meetingId}/details`);
             // console.log('Meeting updated successfully');
           }
         } catch (error) {
@@ -87,9 +78,10 @@ const upDate = data.meeting
       {initialValues && (
         <MeetingForm onSubmit={handleSubmit} mode='edit' initialValues={initialValues} />
       )}
+    
          </Box>
      </>
   )
 }
 
-export default Edit;
+export default Edit 
